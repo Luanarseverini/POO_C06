40c620d5bbe614e45ad17d9081a34d6d7367fd2c
Luanarseverini <luana.roland18@gmail.com>
2025-08-12 14:46:24 -0300

    Add files via upload

---
+++ Exercicio7.Zumbis/src/Zumbi.java
@@ -0,0 +1,14 @@
+public class Zumbi
+{
+
+        int vida;
+        String nome;
+
+    void seAlimentar(){
+        System.out.println("Zumbi se alimentando..."+ nome) ;
+    }
+
+    int mostraVida(){
+        return vida;
+    }
+}
